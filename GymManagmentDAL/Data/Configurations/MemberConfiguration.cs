c685a00ad2ec3b19d5c19199fa24be7d8f13fcfc
Mohamed Anwer <mohamedftooh898@gmail.com>
2025-10-09 14:54:45 +0300

    Entities Configurations

---
+++ GymManagmentDAL/Data/Configurations/MemberConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using GymManagmentDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagmentDAL.Data.Configurations
+{
+    internal class MemberConfiguration : GymUserConfigurations<Member>, IEntityTypeConfiguration<Member>
+    {
+        public new void Configure(EntityTypeBuilder<Member> builder)
+        {
+            builder.Property(x => x.CreatedAt)
+                .HasColumnName("JoinDate")
+                .HasDefaultValueSql("GETDATE()");
+            base.Configure(builder);
+
+        }
+    }
+}
