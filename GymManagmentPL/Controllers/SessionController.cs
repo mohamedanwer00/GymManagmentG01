6e515054c1ce77adc57fe154a2c096c113ad0fa3
Mohamed Anwer <mohamedftooh898@gmail.com>
2025-11-11 18:12:44 +0200

    Session Controller

--- GymManagmentPL/Controllers/SessionController.cs
+++ GymManagmentPL/Controllers/SessionController.cs
@@ -19,15 +19,15 @@ namespace GymManagmentPL.Controllers
             return View(sessions);
         }
 
-        public ActionResult Details(int  id)
+        public ActionResult Details(int id)
         {
-            if(id <= 0)
+            if (id <= 0)
             {
                 TempData["ErrorMessage"] = "Invalid Session Id";
                 return RedirectToAction(nameof(Index));
             }
             var session = _sessionServices.GetSessionDetails(id);
-            if(session == null)
+            if (session == null)
             {
                 TempData["ErrorMessage"] = "Session Not Found";
                 return RedirectToAction(nameof(Index));
@@ -36,6 +36,7 @@ namespace GymManagmentPL.Controllers
             return View(session);
         }
 
+        #region create
         public ActionResult Create()
         {
             LoadDropDowns();
@@ -52,7 +53,7 @@ namespace GymManagmentPL.Controllers
                 return View(CreateSession);
             }
 
-            var result =_sessionServices.CreateSession(CreateSession);
+            var result = _sessionServices.CreateSession(CreateSession);
 
             if (result)
             {
@@ -61,13 +62,93 @@ namespace GymManagmentPL.Controllers
             }
             else
             {
-                LoadDropDowns(); 
+                LoadDropDowns();
                 TempData["ErrorMessage"] = "Srssion Failed To Create";
                 return View(CreateSession);
 
             }
 
         }
+        #endregion
+
+        #region Edit
+        public ActionResult Edit(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid session ID.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var session = _sessionServices.GetSessionToUpdate(id);
+
+            if (session == null)
+            {
+                TempData["ErrorMessage"] = "Session not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            LoadTrainersDropDown();
+            return View(session);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id,UpdateSessionViewModel UpdatedSession)
+        {
+            if (!ModelState.IsValid)
+            {
+                LoadTrainersDropDown();
+                return View(UpdatedSession);
+            }
+
+                var result = _sessionServices.UpdateSession(id,UpdatedSession);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Session updated successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed to update session. Please try again.";
+                LoadTrainersDropDown();
+                return View(UpdatedSession);
+            }
+        }
+        #endregion
+
+        #region Delete
+        public ActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid Session Id";
+                return RedirectToAction(nameof(Index));
+            }
+            var session = _sessionServices.GetSessionDetails(id);
+            if (session ==null)
+            {
+                TempData["ErrorMessage"] = "Session Not Found";
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.SessionId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var result = _sessionServices.DeleteSession(id);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Session Deleted Succesfully";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Session Failed To Delete";
+                return RedirectToAction(nameof(Index));
+            }
+        }
+        #endregion
 
         #region Helper
         private void LoadDropDowns()
@@ -76,7 +157,13 @@ namespace GymManagmentPL.Controllers
             ViewBag.Categories = new SelectList(categories, "Id", "Name");
             var trainers = _sessionServices.GetTrainersForDropdown();
             ViewBag.Trainers = new SelectList(trainers, "Id", "Name");
-        } 
+        }
+
+        private void LoadTrainersDropDown()
+        {
+            var trainers = _sessionServices.GetTrainersForDropdown();
+            ViewBag.Trainers = new SelectList(trainers, "Id", "Name");
+        }
         #endregion
 
     }
